257014247b169100a27ee43153a88fe22a547f2c
BliztRipper <nonthegenocide@me.com>
2023-11-27 03:57:16 +0700

    add carousel

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,25 @@
 import "./App.scss";
+import Slider from "react-slick";
 
 function App() {
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+  };
+
   return (
     <>
       <body>
-        <div class="athlets_container">
-          <div class="header">
+        <article className="athlets_container">
+          <div className="header">
             <header>
               <h1 className="main_header">ATHLETS</h1>
             </header>
           </div>
-          <div class="sub_header-1">
+          <div className="sub_header-1">
             <section>
               <div className="sub_header--wrapper">
                 <div className="sub_header--li">01</div>
@@ -22,7 +31,7 @@ function App() {
               </p>
             </section>
           </div>
-          <div class="sub_header-2">
+          <div className="sub_header-2">
             <section>
               <div className="sub_header--wrapper">
                 <div className="sub_header--li">02</div>
@@ -36,7 +45,7 @@ function App() {
               </p>
             </section>
           </div>
-          <div class="sub_header-3">
+          <div className="sub_header-3">
             <section>
               <div className="sub_header--wrapper">
                 <div className="sub_header--li">03</div>
@@ -48,29 +57,77 @@ function App() {
               </p>
             </section>
           </div>
-          <div class="image-1">
+          <figure className="image-1">
             <picture className="img_container">
               <source
                 media="(min-width:769px)"
                 srcset="../src/assets/fb_desktop.svg"
               />
               <source
-                media="(min-width:321px)"
+                media="(min-width:651px)"
                 srcset="../src/assets/fb_tablet.svg"
               />
               <img src="../src/assets/fb_mobile.svg" alt="football player" />
             </picture>
+          </figure>
+          <div className="image-2"></div>
+          <div className="image-3"></div>
+          <div className="slider_wrapper">
+            <Slider {...settings}>
+              <div className="sub_header-slide">
+                <section>
+                  <div className="sub_header-slide--wrapper">
+                    <div className="sub_header-slide--li">01</div>
+                    <h3 className="sub_header">CONNECTION</h3>
+                  </div>
+                  <div className="paragraph_wrapper">
+                    <p className="paragraph">
+                      Connect with coaches directly, you can ping coaches to
+                      view profile.
+                    </p>
+                  </div>
+                </section>
+              </div>
+              <div className="sub_header-slide">
+                <section>
+                  <div className="sub_header-slide--wrapper">
+                    <div className="sub_header-slide--li">02</div>
+                    <h3 className="sub_header">COLLABORATION</h3>
+                  </div>
+                  <div className="paragraph_wrapper">
+                    <p className="paragraph">
+                      Work with other student athletes to  increase visability.
+                      When you share and like other players videos it will
+                      increase your visability as a player. This is the team
+                      work aspect to Surface 1.
+                    </p>
+                  </div>
+                </section>
+              </div>
+              <div className="sub_header-slide">
+                <section>
+                  <div className="sub_header-slide--wrapper">
+                    <div className="sub_header-slide--li">03</div>
+                    <h3 className="sub_header">GROWTH</h3>
+                  </div>
+                  <div className="paragraph_wrapper">
+                    <p className="paragraph">
+                      Resources and tools for you to get better as a student
+                      Athelte. Access to training classes, tutor sessions, etc 
+                    </p>
+                  </div>
+                </section>
+              </div>
+            </Slider>
           </div>
-          <div class="image-2"></div>
-          <div class="image-3"></div>
-        </div>
-        <div class="player_container">
-          <div class="header">
+        </article>
+        <article className="player_container">
+          <div className="header">
             <header>
               <h1 className="main_header">PLAYERS</h1>
             </header>
           </div>
-          <div class="sub_header-1">
+          <div className="sub_header-1">
             <section>
               <div className="sub_header--wrapper">
                 <div className="sub_header--li">01</div>
@@ -82,7 +139,7 @@ function App() {
               </p>
             </section>
           </div>
-          <div class="sub_header-2">
+          <div className="sub_header-2">
             <section>
               <div className="sub_header--wrapper">
                 <div className="sub_header--li">02</div>
@@ -94,7 +151,7 @@ function App() {
               </p>
             </section>
           </div>
-          <div class="sub_header-3">
+          <div className="sub_header-3">
             <section>
               <div className="sub_header--wrapper">
                 <div className="sub_header--li">03</div>
@@ -105,22 +162,68 @@ function App() {
               </p>
             </section>
           </div>
-          <div class="image-1">
+          <figure className="image-1">
             <picture className="img_container">
               <source
                 media="(min-width:769px)"
                 srcset="../src/assets/bas_desktop.svg"
               />
               <source
-                media="(min-width:321px)"
+                media="(min-width:651px)"
                 srcset="../src/assets/bas_tablet.svg"
               />
               <img src="../src/assets/bas_mobile.svg" alt="basketball player" />
             </picture>
+          </figure>
+          <div className="image-2"></div>
+          <div className="image-3"></div>
+          <div className="slider_wrapper">
+            <Slider {...settings}>
+              <div className="sub_header-slide">
+                <section>
+                  <div className="sub_header-slide--wrapper">
+                    <div className="sub_header-slide--li">01</div>
+                    <h3 className="sub_header">CONNECTION</h3>
+                  </div>
+                  <div className="paragraph_wrapper">
+                    <p className="paragraph">
+                      Connect with talented athlete directly, you can watch
+                      their skills through video showreels directly from Surface
+                      1.
+                    </p>
+                  </div>
+                </section>
+              </div>
+              <div className="sub_header-slide">
+                <section>
+                  <div className="sub_header-slide--wrapper">
+                    <div className="sub_header-slide--li">02</div>
+                    <h3 className="sub_header">COLLABORATION</h3>
+                  </div>
+                  <div className="paragraph_wrapper">
+                    <p className="paragraph">
+                      Work with recruiter to increase your chances of finding
+                      talented athlete.
+                    </p>
+                  </div>
+                </section>
+              </div>
+              <div className="sub_header-slide">
+                <section>
+                  <div className="sub_header-slide--wrapper">
+                    <div className="sub_header-slide--li">03</div>
+                    <h3 className="sub_header">GROWTH</h3>
+                  </div>
+                  <div className="paragraph_wrapper">
+                    <p className="paragraph">
+                      Save your time, recruit proper athlets for your team.
+                    </p>
+                  </div>
+                </section>
+              </div>
+            </Slider>
           </div>
-          <div class="image-2"></div>
-          <div class="image-3"></div>
-        </div>
+        </article>
       </body>
     </>
   );
